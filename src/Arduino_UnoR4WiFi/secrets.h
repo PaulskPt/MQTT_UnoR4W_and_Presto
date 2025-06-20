07397fa2666d10d3cb146495b7835998fcbd983a
Paulus H.J. Schulinck <PaulskPt@users.noreply.github.com>
2025-06-20 11:00:39 +0100

    Update secrets.h of Arduino Uno R4 WiFi
    changes in file secrets.h of Arduino Uno R4 WiFi

--- src/Arduino_UnoR4WiFi/secrets.h
+++ src/Arduino_UnoR4WiFi/secrets.h
@@ -3,6 +3,7 @@
 #define SECRET_TIMEZONE_OFFSET "1" // Europe/Lisbon
 // #define TIMEZONE_OFFSET "-4" // America/New_York
 #define SECRET_NTP_SERVER1 "1.pt.pool.ntp.org"
-#define SECRET_MQTT_BROKER "test.mosquitto.org"
+#define SECRET_MQTT_BROKER "5.196.78.28" // test.mosquitto.org"
 #define SECRET_MQTT_PORT "1883"
+#define SECRET_MQTT_TOPIC "sensors/UnoR4W/ambient"
 
